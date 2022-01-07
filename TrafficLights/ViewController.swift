fb5de123c298a35d1a9c72a6ec2a7928f9133837
onixjk <onixjkk@gmail.ru>
2022-01-07 20:03:46 +0300

    done

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -7,13 +7,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+enum CurrentColor {
+    case red
+    case yellow
+    case green
 }
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var redColor: UIView!
+    @IBOutlet weak var yellowColor: UIView!
+    @IBOutlet weak var greenColor: UIView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    var currentColor = CurrentColor.red
+    private let colorOn: CGFloat = 1
+    private let colorOff: CGFloat = 0.3
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        redColor.alpha = colorOff
+        yellowColor.alpha = colorOff
+        greenColor.alpha = colorOff
+        
+        startButton.layer.cornerRadius = 20
+    }
+
+    override func viewWillLayoutSubviews() {
+        redColor.layer.cornerRadius = redColor.frame.height / 2
+        yellowColor.layer.cornerRadius = yellowColor.frame.height / 2
+        greenColor.layer.cornerRadius = greenColor.frame.height / 2
+    }
+    
+    @IBAction func startButtonPressed() {
+        startButton.setTitle("Next", for: .normal)
+        
+        switch currentColor {
+        case .red:
+            greenColor.alpha = colorOff
+            redColor.alpha = colorOn
+            currentColor = .yellow
+        case .yellow:
+            redColor.alpha = colorOff
+            yellowColor.alpha = colorOn
+            currentColor = .green
+        case .green:
+            yellowColor.alpha = colorOff
+            greenColor.alpha = colorOn
+            currentColor = .red
+        }
+    }
+}
